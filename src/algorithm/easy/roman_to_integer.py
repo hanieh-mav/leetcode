a84575b37fe12a19e69c0767b3b1a715014d7d91
hanieh mahdiabadi <h.mehdiabadi@gmail.com>
2024-02-04 12:23:47 +0330

    solve question and pass all tests for roman to int

--- src/algorithm/easy/roman_to_integer.py
+++ src/algorithm/easy/roman_to_integer.py
@@ -3,4 +3,16 @@
 
 class Solution:
     def romanToInt(self, s: str) -> int:
-        pass
+        roman_dict_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+
+        skip_index = None
+        for i, char in enumerate(s):
+            if skip_index and skip_index == i:
+                continue
+            if i == len(s) - 1 or roman_dict_values[char] >= roman_dict_values[s[i + 1]]:
+                result += roman_dict_values[char]
+            else:
+                result += roman_dict_values[s[i + 1]] - roman_dict_values[s[i]]
+                skip_index = i + 1
+        return result
