653e580733b5ab5f66ac2b11eead4407db4da682
hanieh mahdiabadi <h.mehdiabadi@gmail.com>
2024-02-04 12:13:51 +0330

    solve question and run tests for that

--- src/algorithm/easy/palindrome_number.py
+++ src/algorithm/easy/palindrome_number.py
@@ -1,3 +1,19 @@
+# Description ===> https://leetcode.com/problems/palindrome-number/description/
+
+
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        pass
+    @staticmethod
+    def isPalindrome(x: int) -> bool:
+        if x < 0:
+            return False
+
+        first_x = x
+        result = x % 10
+        x //= 10
+        while x // 10 != 0:
+            result = (result * 10) + (x % 10)
+            x //= 10
+
+        if x != 0:
+            result = (result * 10) + (x % 10)
+        return first_x == result
--- test/algorithm/easy/test_palindrom_number.py
+++ test/algorithm/easy/test_palindrom_number.py
@@ -6,8 +6,18 @@ from src.algorithm.easy.palindrome_number import Solution
 class TestPalindromeNumber(unittest.TestCase):
     def test_case_one(self):
         x = 121
-        res = Solution().isPalindrome(x)
+        res = Solution.isPalindrome(x)
         assert res == True
+
+    def test_case_two(self):
+        x = -121
+        res = Solution.isPalindrome(x)
+        assert res is False
+
+    def test_case_three(self):
+        x = 10
+        res = Solution.isPalindrome(x)
+        assert res is False
 
 
 if __name__ == '__main__':
