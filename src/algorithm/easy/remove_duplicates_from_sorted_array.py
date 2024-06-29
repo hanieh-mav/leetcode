a02bc50d6c025dc5c4c7db0210b4deb95a8924b9
hanieh mahdiabadi <h.mehdiabadi@gmail.com>
2024-06-29 17:50:52 +0330

    try to solve question and write test for that

--- src/algorithm/easy/remove_duplicates_from_sorted_array.py
+++ src/algorithm/easy/remove_duplicates_from_sorted_array.py
@@ -1,7 +1,21 @@
-
-
-
-
 class Solution:
+
     def removeDuplicates(self, nums: list[int]) -> int:
-        pass
+        if len(nums) == 1:
+            return 1
+        if not len(nums):
+            return 0
+
+        pointer_1, pointer_2 = 0, 1
+        counter = 1
+        while pointer_1 is not None and pointer_2 is not None and not (
+                pointer_1 == len(nums) or pointer_2 == len(nums)):
+            if nums[pointer_1] == nums[pointer_2]:
+                pointer_2 += 1
+            if nums[pointer_1] < nums[pointer_2]:
+                pointer_1 += 1
+                nums[pointer_1] = nums[pointer_2]
+                counter += 1
+                pointer_2 += 1
+
+        return counter +1
--- test/algorithm/easy/test_remove_duplicates_from_sorted_array.py
+++ test/algorithm/easy/test_remove_duplicates_from_sorted_array.py
@@ -6,13 +6,23 @@ from src.algorithm.easy.remove_duplicates_from_sorted_array import Solution
 class TestRemoveDuplicates(unittest.TestCase):
 
     def test_case_one(self):
-        nums = [1, 1, 2]
-        k, res = Solution().removeDuplicates(nums)
+        nums = [1, 1]
+        k = Solution().removeDuplicates(nums)
         assert k == 2
-        assert res == [1, 2, 2]
 
     def test_case_two(self):
         nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
-        k, res = Solution().removeDuplicates(nums)
+        k = Solution().removeDuplicates(nums)
         assert k == 5
-        assert res == [0, 1, 2, 3, 4, 2, 2, 3, 3, 4]
+
+
+    def test_case_three(self):
+        nums = [0]
+        k = Solution().removeDuplicates(nums)
+        assert k == 1
+
+
+    def test_case_four(self):
+        nums = []
+        k = Solution().removeDuplicates(nums)
+        assert k == 0
